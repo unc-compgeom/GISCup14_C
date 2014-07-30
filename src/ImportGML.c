54595426d804ed4030d9a7eaa1f5f86209db47c1
Vance Miller <vbmiller@cs.unc.edu>
2014-07-30 19:02:18 -0400

    trying to debug

--- src/ImportGML.c
+++ src/ImportGML.c
@@ -16,7 +16,7 @@ struct PointsList * importGML_readFile(char fileName[]) {
 	while(1) {
 		while(1) {
 			fscanf(fp, "%c", &c);
-			printf("%c", c);
+			fputs("%c", c);
 			if (c == '>') {
 				break;
 			}
