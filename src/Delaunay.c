0a45eebd488b26426cf6c098399d278b47dcf3c5
Vance Miller <vbmiller@cs.unc.edu>
2014-07-30 14:13:30 -0400

    output strings

--- src/Delaunay.c
+++ src/Delaunay.c
@@ -3,6 +3,7 @@
 #include "Subdivision.h"
 #include "Delaunay.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 ///////////////// DELAUNAY TRIANGULATION /////////////////
 struct Subdivision * delaunay_triangulate(struct Point points[], int numPoints) {
@@ -10,8 +11,10 @@ struct Subdivision * delaunay_triangulate(struct Point points[], int numPoints) 
 	struct QuadEdge *qe = (struct QuadEdge*) malloc(sizeof(struct QuadEdge));;
 	subdivision_construct(s, qe);
 	int i;
+	printf("consructed subdivision\n");
 	for (i = 0; i < numPoints; i++) {
 		subdivision_insertSite(s, &points[i]);
+		printf("inserted point %d\n", i);
 	}
 	return s;
 }
