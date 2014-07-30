98c94ef665098038e95b008473d982901dcafe7c
Vance Miller <vbmiller@cs.unc.edu>
2014-07-30 12:54:27 -0400

    main

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,10 @@
 #include <stdio>
 #include "Delaunay.h"
+#include "Subdivision.h"
 
 int main() {
 	printf("test");
+	struct Subdivision *triangulation;
+	struct Point points[10];
+	triangulation = delaunay_triangulate(points, 10);
 }
